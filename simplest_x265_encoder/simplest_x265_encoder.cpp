f28f6814f444e1a1da294c949c1a5d477230d2ca
Bartek Zdanowski <bartek.zdanowski@gmail.com>
2022-04-04 11:37:36 +0200

    Migrate to CMake and external x265

--- simplest_x265_encoder/simplest_x265_encoder.cpp
+++ simplest_x265_encoder/simplest_x265_encoder.cpp
@@ -1,15 +1,15 @@
 /**
- * ×î¼òµ¥µÄ»ùÓÚX265µÄÊÓÆµ±àÂëÆ÷
+ * ï¿½ï¿½òµ¥µÄ»ï¿½ï¿½ï¿½X265ï¿½ï¿½ï¿½ï¿½Æµï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
  * Simplest X265 Encoder
  *
- * À×Ïöæè Lei Xiaohua
+ * ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ Lei Xiaohua
  * leixiaohua1020@126.com
- * ÖÐ¹ú´«Ã½´óÑ§/Êý×ÖµçÊÓ¼¼Êõ
+ * ï¿½Ð¹ï¿½ï¿½ï¿½Ã½ï¿½ï¿½Ñ§/ï¿½ï¿½ï¿½Öµï¿½ï¿½Ó¼ï¿½ï¿½ï¿½
  * Communication University of China / Digital TV Technology
  * http://blog.csdn.net/leixiaohua1020
  *
- * ±¾³ÌÐò¿ÉÒÔYUV¸ñÊ½µÄÏñËØÊý¾Ý±àÂëÎªH.265ÂëÁ÷£¬ÊÇ×î¼òµ¥µÄ
- * »ùÓÚlibx265µÄÊÓÆµ±àÂëÆ÷
+ * ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½YUVï¿½ï¿½Ê½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ý±ï¿½ï¿½ï¿½ÎªH.265ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½òµ¥µï¿½
+ * ï¿½ï¿½ï¿½ï¿½libx265ï¿½ï¿½ï¿½ï¿½Æµï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
  *
  * This software encode YUV data to H.265 bitstream.
  * It's the simplest encoder example based on libx265.
@@ -17,145 +17,158 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#if defined ( __cplusplus)
-extern "C"
-{
-#include "x265.h"
+#if defined(__cplusplus)
+extern "C" {
+#include <x265.h>
 };
 #else
-#include "x265.h"
+#include <x265.h>
 #endif
 
-int main(int argc, char** argv){
-	int i,j;
-	FILE *fp_src=NULL;
-	FILE *fp_dst=NULL;
-	int y_size;
-	int buff_size;
-	char *buff=NULL;
-	int ret;
-	x265_nal *pNals=NULL;
-	uint32_t iNal=0;
+int main(int argc, char **argv) {
+  int i, j;
+  FILE *fp_src = NULL;
+  FILE *fp_dst = NULL;
+  int y_size;
+  int buff_size;
+  char *buff = NULL;
+  int ret;
+  x265_nal *pNals = NULL;
+  uint32_t iNal = 0;
 
-	x265_param* pParam=NULL;
-	x265_encoder* pHandle=NULL;
-	x265_picture *pPic_in=NULL;
+  x265_param *pParam = NULL;
+  x265_encoder *pHandle = NULL;
+  x265_picture *pPic_in = NULL;
 
-	//Encode 50 frame
-	//if set 0, encode all frame
-	int frame_num=50;
-	int csp=X265_CSP_I420;
-	int width=640,height=360;
+  // Encode 50 frame
+  // if set 0, encode all frame
+  int frame_num = 50;
+  int csp = X265_CSP_I420;
+  int width = 640, height = 360;
 
-	fp_src=fopen("../cuc_ieschool_640x360_yuv420p.yuv","rb");
-	//fp_src=fopen("../cuc_ieschool_640x360_yuv444p.yuv","rb");
+  fp_src = fopen("../cuc_ieschool_640x360_yuv420p.yuv", "rb");
+  // file below flickers, needs to be investigated
+  //   fp_src=fopen("../cuc_ieschool_640x360_yuv444p.yuv","rb");
 
-	fp_dst=fopen("cuc_ieschool.h265","wb");
-	//Check
-	if(fp_src==NULL||fp_dst==NULL){
-		return -1;
-	}
+  fp_dst = fopen("cuc_ieschool.h265", "wb");
+  // Check
+  if (fp_src == NULL || fp_dst == NULL) {
+    return -1;
+  }
 
-	pParam=x265_param_alloc();
-	x265_param_default(pParam);
-	pParam->bRepeatHeaders=1;//write sps,pps before keyframe
-	pParam->internalCsp=csp;
-	pParam->sourceWidth=width;
-	pParam->sourceHeight=height;
-	pParam->fpsNum=25;
-	pParam->fpsDenom=1;
-	//Init
-	pHandle=x265_encoder_open(pParam);
-	if(pHandle==NULL){
-		printf("x265_encoder_open err\n");
-		return 0;
-	}
-	y_size = pParam->sourceWidth * pParam->sourceHeight;
+  pParam = x265_param_alloc();
+  x265_param_default(pParam);
+  pParam->bRepeatHeaders = 1; // write sps,pps before keyframe
+  pParam->internalCsp = csp;
+  pParam->sourceWidth = width;
+  pParam->sourceHeight = height;
+  pParam->fpsNum = 25;
+  pParam->fpsDenom = 1;
+  // Init
+  pHandle = x265_encoder_open(pParam);
+  if (pHandle == NULL) {
+    printf("x265_encoder_open err\n");
+    return 0;
+  }
+  y_size = pParam->sourceWidth * pParam->sourceHeight;
 
-	pPic_in = x265_picture_alloc();
-	x265_picture_init(pParam,pPic_in);
-	switch(csp){
-	case X265_CSP_I444:{
-		buff=(char *)malloc(y_size*3);
-		pPic_in->planes[0]=buff;
-		pPic_in->planes[1]=buff+y_size;
-		pPic_in->planes[2]=buff+y_size*2;
-		pPic_in->stride[0]=width;
-		pPic_in->stride[1]=width;
-		pPic_in->stride[2]=width;
-		break;
-					   }
-	case X265_CSP_I420:{
-		buff=(char *)malloc(y_size*3/2);
-		pPic_in->planes[0]=buff;
-		pPic_in->planes[1]=buff+y_size;
-		pPic_in->planes[2]=buff+y_size*5/4;
-		pPic_in->stride[0]=width;
-		pPic_in->stride[1]=width/2;
-		pPic_in->stride[2]=width/2;
-		break;
-					   }
-	default:{
-		printf("Colorspace Not Support.\n");
-		return -1;
-			}
-	}
-	
-	//detect frame number
-	if(frame_num==0){
-		fseek(fp_src,0,SEEK_END);
-		switch(csp){
-		case X265_CSP_I444:frame_num=ftell(fp_src)/(y_size*3);break;
-		case X265_CSP_I420:frame_num=ftell(fp_src)/(y_size*3/2);break;
-		default:printf("Colorspace Not Support.\n");return -1;
-		}
-		fseek(fp_src,0,SEEK_SET);
-	}
+  pPic_in = x265_picture_alloc();
+  x265_picture_init(pParam, pPic_in);
+  switch (csp) {
+  case X265_CSP_I444: {
+    buff = (char *)malloc(y_size * 3);
+    pPic_in->planes[0] = buff;
+    pPic_in->planes[1] = buff + y_size;
+    pPic_in->planes[2] = buff + y_size * 2;
+    pPic_in->stride[0] = width;
+    pPic_in->stride[1] = width;
+    pPic_in->stride[2] = width;
+    break;
+  }
+  case X265_CSP_I420: {
+    buff = (char *)malloc(y_size * 3 / 2);
+    pPic_in->planes[0] = buff;
+    pPic_in->planes[1] = buff + y_size;
+    pPic_in->planes[2] = buff + y_size * 5 / 4;
+    pPic_in->stride[0] = width;
+    pPic_in->stride[1] = width / 2;
+    pPic_in->stride[2] = width / 2;
+    break;
+  }
+  default: {
+    printf("Colorspace Not Support.\n");
+    return -1;
+  }
+  }
 
-	//Loop to Encode
-	for( i=0;i<frame_num;i++){
-		switch(csp){
-		case X265_CSP_I444:{
-			fread(pPic_in->planes[0],1,y_size,fp_src);		//Y
-			fread(pPic_in->planes[1],1,y_size,fp_src);		//U
-			fread(pPic_in->planes[2],1,y_size,fp_src);		//V
-			break;}
-		case X265_CSP_I420:{
-			fread(pPic_in->planes[0],1,y_size,fp_src);		//Y
-			fread(pPic_in->planes[1],1,y_size/4,fp_src);	//U
-			fread(pPic_in->planes[2],1,y_size/4,fp_src);	//V
-			break;}
-		default:{
-			printf("Colorspace Not Support.\n");
-			return -1;}
-		}
+  // detect frame number
+  if (frame_num == 0) {
+    fseek(fp_src, 0, SEEK_END);
+    switch (csp) {
+    case X265_CSP_I444:
+      frame_num = ftell(fp_src) / (y_size * 3);
+      break;
+    case X265_CSP_I420:
+      frame_num = ftell(fp_src) / (y_size * 3 / 2);
+      break;
+    default:
+      printf("Colorspace Not Support.\n");
+      return -1;
+    }
+    fseek(fp_src, 0, SEEK_SET);
+  }
 
-		ret=x265_encoder_encode(pHandle,&pNals,&iNal,pPic_in,NULL);	
-		printf("Succeed encode %5d frames\n",i);
+  // Loop to Encode
+  for (i = 0; i < frame_num; i++) {
+    switch (csp) {
+    case X265_CSP_I444: {
+      fread(pPic_in->planes[0], 1, y_size, fp_src); // Y
+      fread(pPic_in->planes[1], 1, y_size, fp_src); // U
+      fread(pPic_in->planes[2], 1, y_size, fp_src); // V
+      break;
+    }
+    case X265_CSP_I420: {
+      fread(pPic_in->planes[0], 1, y_size, fp_src);     // Y
+      fread(pPic_in->planes[1], 1, y_size / 4, fp_src); // U
+      fread(pPic_in->planes[2], 1, y_size / 4, fp_src); // V
+      break;
+    }
+    default: {
+      printf("Colorspace Not Support.\n");
+      return -1;
+    }
+    }
 
-		for(j=0;j<iNal;j++){
-			fwrite(pNals[j].payload,1,pNals[j].sizeBytes,fp_dst);
-		}	
-	}
-	//Flush Decoder
-	while(1){
-		ret=x265_encoder_encode(pHandle,&pNals,&iNal,NULL,NULL);
-		if(ret==0){
-			break;
-		}
-		printf("Flush 1 frame.\n");
+    ret = x265_encoder_encode(pHandle, &pNals, &iNal, pPic_in, NULL);
+    if (ret > 0) {
+      printf("Succeed encode %5d frames\n", i);
+      printf("Frame buffered %s\n", (ret == 0) ? "yes" : "no");
+    }
 
-		for(j=0;j<iNal;j++){
-			fwrite(pNals[j].payload,1,pNals[j].sizeBytes,fp_dst);
-		}
-	}
-	
-	x265_encoder_close(pHandle);
-	x265_picture_free(pPic_in);
-	x265_param_free(pParam);
-	free(buff);
-	fclose(fp_src);
-	fclose(fp_dst);
-	
-	return 0;
+    for (j = 0; j < iNal; j++) {
+      fwrite(pNals[j].payload, 1, pNals[j].sizeBytes, fp_dst);
+    }
+  }
+  // Flush Decoder
+  while (1) {
+    ret = x265_encoder_encode(pHandle, &pNals, &iNal, NULL, NULL);
+    if (ret == 0) {
+      break;
+    }
+    printf("Flush 1 frame.\n");
+
+    for (j = 0; j < iNal; j++) {
+      fwrite(pNals[j].payload, 1, pNals[j].sizeBytes, fp_dst);
+    }
+  }
+
+  x265_encoder_close(pHandle);
+  x265_picture_free(pPic_in);
+  x265_param_free(pParam);
+  free(buff);
+  fclose(fp_src);
+  fclose(fp_dst);
+
+  printf("Done\n");
+  return 0;
 }
